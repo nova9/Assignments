7fa56e7a0ecb2388bfa5290a57d273848b7e83ad
nova9 <thathsaramadhhusha@gmail.com>
2023-08-21 16:07:02 +0530

    Add Makefile and refactor 22001158.c for readability and efficiency
    New Makefile was added for easier build and run of the program 22001158.c. Refactored 22001158.c to improve code readability and efficiency. Removed redundant traversal and display methods, replacing them with 'print_tree' to unify traversal and display logic. Insertions now use an array instead of manually entering each node, increasing the efficiency of the code for testing. This simplifies code structure and makes further development and debugging easier.

--- 1201_DSA/practical11/22001158.c
+++ 1201_DSA/practical11/22001158.c
@@ -3,28 +3,12 @@
 #include <ctype.h>
 #include <errno.h>
 
-typedef enum TraverseType {
-    PREORDER,
-    INORDER,
-    POSTORDER,
-
-} TraverseType;
-
 typedef struct Node Node;
 struct Node {
     int key;
     Node *left;
     Node *right;
 };
-
-void print_preorder(Node *node) {
-    if (node == NULL) {
-        return;
-    }
-    printf("%d  ", node->key);
-    print_preorder(node->left);
-    print_preorder(node->right);
-}
 
 void print_inorder(Node *node) {
     if (node == NULL) {
@@ -33,40 +17,6 @@ void print_inorder(Node *node) {
     print_inorder(node->left);
     printf("%d  ", node->key);
     print_inorder(node->right);
-}
-
-void print_postorder(Node *node) {
-    if (node == NULL) {
-        return;
-    }
-    print_postorder(node->left);
-    print_postorder(node->right);
-    printf("%d  ", node->key);
-}
-
-void display(Node *node, int method) {
-    switch (method) {
-        case PREORDER:
-            print_preorder(node);
-            break;
-        case INORDER:
-            print_inorder(node);
-            break;
-        case POSTORDER:
-            print_postorder(node);
-            break;
-        default:
-            printf("Unknown method");
-    }
-    printf("\n");
-}
-
-Node *create_node(int key) {
-    Node *node = malloc(sizeof(Node));
-    node->key = key;
-    node->left = NULL;
-    node->right = NULL;
-    return node;
 }
 
 int find_height(Node *node) {
@@ -84,36 +34,43 @@ int find_height(Node *node) {
     }
 }
 
-void print_tree(Node *tree, int indent) {
-    if (tree != NULL) {
-        print_tree(tree->right, indent + 5);
-        for (int i = 0; i < indent; i++)
-            printf(" ");
-        printf("%d\n", tree->key);
-        print_tree(tree->left, indent + 5);
-    }
-}
-
 int get_balance_factor(Node *node) {
     return find_height(node->left) - find_height(node->right);
 }
 
+void print_tree(Node *node, int indent) {
+    if (node != NULL) {
+        print_tree(node->right, indent + 8);
+        for (int i = 0; i < indent; i++)
+            printf(" ");
+        printf("%d (%d)\n", node->key, get_balance_factor(node));
+        print_tree(node->left, indent + 8);
+    }
+}
+
+Node *create_node(int key) {
+    Node *node = malloc(sizeof(Node));
+    node->key = key;
+    node->left = NULL;
+    node->right = NULL;
+    return node;
+}
+
 Node *right_rotate(Node *y) {
-    struct Node *x = y->left;
-    struct Node *T2 = x->right;
+    Node *x = y->left;
+    Node *T2 = x->right;
     x->right = y;
     y->left = T2;
     return x;
 }
 
 Node *left_rotate(Node *x) {
-    struct Node *y = x->right;
-    struct Node *T2 = y->left;
+    Node *y = x->right;
+    Node *T2 = y->left;
     y->left = x;
     x->right = T2;
     return y;
 }
-
 
 Node *insert(Node *node, int key) {
     if (node == NULL)
@@ -149,31 +106,14 @@ Node *insert(Node *node, int key) {
 
 int main() {
     Node *root = NULL;
-    while (1) {
-        char input[300];
-        printf("Current tree: \n");
+
+    int array[] = {50, 30, 70, 20, 40, 60, 80, 10, 25, 35, 45, 55, 65, 75, 90};
+    int array_length = sizeof(array) / sizeof(int);
+
+    for (int i = 0; i < array_length; i++) {
+        root = insert(root, array[i]);
+        printf("tree after inserting %d\n", array[i]);
         print_tree(root, 0);
-        printf("Enter a value to insert: ");
-        scanf("%s", input);
-
-        errno = 0;
-        int number = strtol(input, NULL, 10);
-        if (errno != 0) {
-            break;
-        }
-        root = insert(root, number);
+        printf("\n\n");
     }
-
-//    delete_node(&root, 71);
-
-    printf("size = %d\n", find_size(root));
-    printf("height = %d\n", find_height(root));
-
-//    display(root, INORDER);
-
-//    mirror(root);
-//
-//    display(root, INORDER);
-
-//    print_tree(root, 0);
 }
